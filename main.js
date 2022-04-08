e336288e2621c4ef680fdc78909475c5aafb9b48
Paolo Di Martino <paolo.dimartino@outlook.it>
2022-04-08 20:15:38 +0000

    Stylized the table, started adding an event listener for clicks on days.

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ var $submitBtn = document.querySelector('.submit-btn');
 var $addEntryBtn = document.querySelector('.button-add-entry');
 var $overlay = document.querySelector('.overlay');
 var $modalContent = document.querySelector('.modal-content');
+var $dayOfWeekContainer = document.querySelector('.day-of-week-container');
 
 $addEntryBtn.addEventListener('click', function () {
   $overlay.classList.remove('hidden');
   $modalContent.classList.remove('hidden');
 
+});
+
+$dayOfWeekContainer.addEventListener('click', function () {
+  console.log('clicked');
 });
